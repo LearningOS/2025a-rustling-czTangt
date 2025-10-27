aa3082b85a2c09f30b100bf59667822e9881b81e
czTangt <cz.tangt@gmail.com>
2025-10-27 18:50:28 +0800

    finish structs and enums

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,14 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
         // Something goes here...
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> u32 {
         // Something goes here...
+        (self.weight_in_grams as u32) * (cents_per_gram as u32)
     }
 }
 
